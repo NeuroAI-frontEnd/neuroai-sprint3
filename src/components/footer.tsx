bf16182608b44e744c6db4a33deed6e0fa1c6064
CarolinaaFontes <acfonts1@gmail.com>
2025-10-01 20:40:56 -0300

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-200 py-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>© 2025 NeuroAI. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
