60390c16b81cf593976a03ebfd3086836ee6b8e2
CarolinaaFontes <acfonts1@gmail.com>
2025-10-01 20:57:17 -0300

    feat: add home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className="container mx-auto text-center">
+      <h2 className="text-3xl font-bold my-4">Bem-vindo ao NeuroAI</h2>
+      <p>Projeto de agendamento e gestão de consultas médicas.</p>
+    </section>
+  );
+}
