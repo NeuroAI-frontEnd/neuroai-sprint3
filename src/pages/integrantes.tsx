56f2eef467bacd4cd0816bde94048b322657806c
CarolinaaFontes <acfonts1@gmail.com>
2025-10-01 23:16:16 -0300

    feat: add integrantes-pages

---
+++ src/pages/integrantes.tsx
@@ -0,0 +1,45 @@
+export default function Integrantes() {
+  const membros = [
+    {
+      nome: "Ana Carolina Pereira Fontes",
+      turma: "1TDSPV",
+      rm: "562145",
+      foto: "/Carolina_ft.jpg",
+    },
+    {
+      nome: "Daniel Nicolas Leoterio",
+      turma: "1TDSPV",
+      rm: "562186",
+      foto: "/Daniel.jpg",
+    },
+    {
+      nome: "Matheus Moya de Oliveira",
+      turma: "1TDSPV",
+      rm: "562822",
+      foto: "/Matheus.jpg",
+    },
+  ];
+
+  return (
+    <section className="container mx-auto text-center">
+      <h1 className="text-3xl font-bold my-6">Integrantes</h1>
+      <div className="grid gap-6 md:grid-cols-3">
+        {membros.map((membro, i) => (
+          <div
+            key={i}
+            className="bg-white shadow rounded-lg p-4 flex flex-col items-center"
+          >
+            <img
+              src={membro.foto}
+              alt={`Foto de ${membro.nome}`}
+              className="w-32 h-32 object-cover rounded-full mb-4"
+            />
+            <h2 className="text-lg font-semibold">{membro.nome}</h2>
+            <p>Turma: {membro.turma}</p>
+            <p>RM: {membro.rm}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
